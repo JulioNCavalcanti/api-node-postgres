bc8cd614cd94637719d8178b38db16eb452712b8
Júlio Cavalcanti <jnc.nunescavalcanti@outlook.com>
2025-04-14 21:51:38 -0300

    feat: add updateUser function to update user details in the database

--- queries.js
+++ queries.js
@@ -40,3 +40,15 @@ const createUser = (req, res) => {
         res.status(201).send(`User added with ID: ${results.rows[0].id}`)
     })
 }
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id)
+    const { name, email } = req.body
+
+    pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [name, email, id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).send(`User modified with ID: ${id}`)
+    })
+}
