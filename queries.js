fb3c1fefc02127fe071dd20ca759f732566bfb33
Júlio Cavalcanti <jnc.nunescavalcanti@outlook.com>
2025-04-14 21:53:25 -0300

    feat: add deleteUser function to remove a user from the database by ID

--- queries.js
+++ queries.js
@@ -52,3 +52,14 @@ const updateUser = (req, res) => {
         res.status(200).send(`User modified with ID: ${id}`)
     })
 }
+
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).send(`User deleted with ID: ${id}`)
+    })
+}
