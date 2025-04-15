8665a1437c64150c1f6e3aa1665e2fe9ae4a8be4
Júlio Cavalcanti <jnc.nunescavalcanti@outlook.com>
2025-04-14 21:42:08 -0300

    feat: add getUsers function to fetch and return users from the database

--- queries.js
+++ queries.js
@@ -9,3 +9,12 @@ const pool = new Pool({
     password: process.env.PASSWORD,
     port: process.env.PORT,
 })
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
