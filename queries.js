f219c584047ce3c1892992d7036cde13866cca9f
Júlio Cavalcanti <jnc.nunescavalcanti@outlook.com>
2025-04-14 21:45:16 -0300

    feat: add getUserById function to fetch a user by ID from the database

--- queries.js
+++ queries.js
@@ -18,3 +18,14 @@ const getUsers = (request, response) => {
         response.status(200).json(results.rows)
     })
 }
+
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+}
