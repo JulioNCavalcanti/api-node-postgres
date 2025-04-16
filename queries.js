16193995e26d4483de859b3ad8a2da935745b5f7
Júlio Cavalcanti <jnc.nunescavalcanti@outlook.com>
2025-04-15 21:02:23 -0300

    chore: update environment variables for database connection

--- queries.js
+++ queries.js
@@ -3,11 +3,11 @@ dotenv.config()
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_DOCKER_PORT,
 })
 
 const getUsers = (request, response) => {
